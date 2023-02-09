4ca8315bc8b3eb9a577c40e281a6f73c6ee6a743
ferreiramx <angel@ferreira.mx>
2023-02-09 00:20:38 -0600

    Refactor

--- home/components/desktop/wm/qtile/modules/layouts.nix
+++ home/components/desktop/wm/qtile/modules/layouts.nix
@@ -4,6 +4,18 @@ from libqtile.config import Match
 from modules.theme import theme
 
 layouts = [
+    layout.MonadTall(
+        ratio=0.7,
+        max_ratio=0.75,
+        min_ratio=0.5,
+        new_client_position="bottom",
+        margin=theme["windows"]["margin"]["x"],
+        border_width=theme["windows"]["borders"]["width"],
+        singel_margin=theme["windows"]["margin"]["x"],
+        singel_border_width=theme["windows"]["borders"]["width"],
+        border_focus=theme["colors"]["bg4"],
+        border_normal=theme["colors"]["bg3"],
+    ),
     layout.MonadThreeCol(
         ratio=0.5,
         min_ratio=0.34,
@@ -17,18 +29,6 @@ layouts = [
             int(theme["screen"]["width"] / 5),
         ],
         border_width=theme["windows"]["borders"]["width"],
-        border_focus=theme["colors"]["bg4"],
-        border_normal=theme["colors"]["bg3"],
-    ),
-    layout.MonadTall(
-        ratio=0.7,
-        max_ratio=0.75,
-        min_ratio=0.5,
-        new_client_position="bottom",
-        margin=theme["windows"]["margin"]["x"],
-        border_width=theme["windows"]["borders"]["width"],
-        singel_margin=theme["windows"]["margin"]["x"],
-        singel_border_width=theme["windows"]["borders"]["width"],
         border_focus=theme["colors"]["bg4"],
         border_normal=theme["colors"]["bg3"],
     ),
