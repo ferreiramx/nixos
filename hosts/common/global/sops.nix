5bccdd84d2e620cd55ac9008140282f9b4221300
ferreiramx <angel@ferreira.mx>
2023-04-03 14:15:47 -0600

    Add key

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -5,6 +5,7 @@
 
   sops = {
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    age.keyFile = "/home/aferreira/.config/sops/age/keys.txt";
     defaultSopsFile = ./secrets.yml;
   };
 }
